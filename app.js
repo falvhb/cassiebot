8dc350baba3468525f9eb4097dbc92308227bf9b
FedericoElles <github@furikuri.de>
2016-04-05 10:20:52 +0200

    made secrets bind to process.env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+var bot = new builder.BotConnectorBot({ appId: process.env.appId, appSecret: process.env.appSecret });
 bot.add('/', function (session) {
     session.send('Hello World');
 });
@@ -10,6 +10,12 @@ bot.add('/', function (session) {
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+server.get('/', function(req, res, next) {
+  res.send('hello ' + process.env.appId);
+  next();
+})
+
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
