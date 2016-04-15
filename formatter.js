89664c755eae535c942b428de9d7f0c6b39312c2
Friedrich Lasnia <f.lasnia@vhb.de>
2016-04-15 13:54:03 +0200

    Fixed Regex parameter

--- formatter.js
+++ formatter.js
@@ -5,8 +5,8 @@ function ReplaceHighlightingCharacters(text, beginStr, endStr)
     // Replace all occurrences of U+E000 (begin highlighting) with
     // beginStr. Replace all occurrences of U+E001 (end highlighting)
     // with endStr.
-    var regexBegin = new RegExp("\uE000", " **");
-    var regexEnd = new RegExp("\uE001", "** ");
+    var regexBegin = new RegExp("\uE000", "g");
+    var regexEnd = new RegExp("\uE001", "g");
             
     return text.replace(regexBegin, beginStr).replace(regexEnd, endStr);
 }
@@ -38,7 +38,7 @@ formatter.toSearchResultsList = function(data, searchTerm){
 	addLine('** Suchergebnisse für: '+searchTerm+':**');
 	data.forEach(function(row){
 		addLine(++i + '. ' + row.title + ' - [Artikel öffnen](' + row.link + ')' + ' *(' + row.ago + ')*');
-		addLine('> ' + ReplaceHighlightingCharacters(row.summary, '`', '`'));
+		addLine('> ' + ReplaceHighlightingCharacters(row.summary, ' **', '** '));
         addLine('');
 	});
     addLine('');
